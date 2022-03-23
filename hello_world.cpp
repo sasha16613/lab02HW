7ca2f66159033e53d1db4309fa0676998a7dfede
Your Name <sasha.nov0707@gmail.com>
2022-03-23 22:49:57 +0300

    changed hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-using namespace std;
-int main () {
-    std::cout << "hello world!";
+#include <string>
+
+int main() {
+    std::string n;
+    std::cout << "Введите имя: ";
+    std::cin >> n;
+    std::cout << "Hello world " << n << std::endl;
 }
