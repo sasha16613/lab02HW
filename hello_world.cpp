664434dd97584f0056a12c94804cf0e691d030ad
Your Name <sasha.nov0707@gmail.com>
2022-03-24 21:40:25 +0300

    changed codestyle hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 int main() {
     std::string n; //name
     std::cout << "Введите имя: ";
-    std::cin >> n >> std::endl; //имя
+    std::cin >> n >> std::endl;
     std::cout << "Hello world " << n << std::endl;
+=======
+int
+main()
+{
+  std::string n; // name
+  std::cout << "Введите имя: ";
+  std::cin >> n >> std::endl;
+  std::cout << "Hello world " << n << std::endl;
+>>>>>>> changed codestyle hello_world
 }
