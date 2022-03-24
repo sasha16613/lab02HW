80a0e6e2f7fc2c10ec46cae4d6ba7453ba061ec0
AlexNov <90476843+sasha16613@users.noreply.github.com>
2022-03-25 00:31:36 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@
 int main() {
     std::string n; //name
     std::cout << "Введите имя: ";
-    std::cin >> n>> std::endl; //имя
+    std::cin >> n>> std::endl;
     std::cout << "Hello world " << n << std::endl;
 }
