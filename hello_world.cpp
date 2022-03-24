ea2b34ffd6537756d63cb7a9f30d86f03db71a81
Your Name <sasha.nov0707@gmail.com>
2022-03-24 20:10:57 +0300

    changed hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-    std::string n;
+    std::string n; //name
     std::cout << "Введите имя: ";
     std::cin >> n>> std::endl;
     std::cout << "Hello world " << n << std::endl;
