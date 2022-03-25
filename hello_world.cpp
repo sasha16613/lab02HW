959cd5ee3253898e35734d01a2bff5932e0a0a76
Your Name <sasha.nov0707@gmail.com>
2022-03-25 07:59:18 +0300

    chenged style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,24 @@
-#include <iostream>
 #include "iostream"
+#include <iostream>
 using namespace std;
 
+<<<<<<< HEAD
 int main() {
     std::string n;
     std::cout << "Введите имя: ";
 <<<<<<< HEAD
-    std::cin >> n;
+    std::cin >> n>> std::endl;
 =======
     std::cin >> n>> std::endl;
 >>>>>>> 80a0e6e2f7fc2c10ec46cae4d6ba7453ba061ec0
     std::cout << "Hello world " << n << std::endl;
+=======
+int
+main()
+{
+  std::string n;
+  std::cout << "Введите имя: ";
+  std::cin >> n;
+  std::cout << "Hello world " << n << std::endl;
+>>>>>>> chenged style
 }
